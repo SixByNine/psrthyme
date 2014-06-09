ff924083a1be027e7d29b922eb03f7976a75d3cf
Michael Keith <mkeith@pulsarastronomy.net>
2014-06-09 15:02:56 +0100

    Comit

---
+++ src/programs/psrthyme-test.cpp
@@ -0,0 +1,37 @@
+//  Copyright (C) 2014 Michael J. Keith, University of Manchester
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+#include <inttypes.h>
+#include <TKlog.h>
+
+#include <psrthyme/psrthyme.hpp>
+#include "psrthyme-test.hpp"
+
+
+int main(int argc, char** argv){
+
+   PsrthymeTemplate::Ptr tmpl = PsrthymeTemplate::read("../../../kwikfit/examples/psr1.tmpl");
+
+   tmpl->write(std::cout);
+
+   PsrthymeProfile::Ptr obs = PsrthymeProfile::Ptr(new PsrthymeProfile("../../../kwikfit/examples/psr1.asc"));
+		 obs->read();
+
+   PsrthymeFitter::Ptr fitter = PsrthymeFitter::Ptr(new PsrthymeFitter());
+   fitter->clear();
+   fitter->addItteration(4,false,false);
+   fitter->addItteration(4,true,false);
+   fitter->addItteration(8,true,true);
+   fitter->addItteration(32,true,true);
+   fitter->addItteration(128,true,true);
+   fitter->setTemplate(tmpl);
+
+   PsrthymeResult::Ptr result = fitter->fitTo(obs);
+   PsrthymePlotter plt("2/xs");
+   plt.plot(result);
+   logmsg("PHASE = %lf",result->phase);
+   logmsg("ERROR = %lf",result->error);
+
+   return 0;
+}
